2a49cad13f9576148738f9982d658cfa29444efa
Bryan Alexander <bryanalex2017@gmail.com>
2023-12-04 09:29:36 -0600

    #

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import { users } from "./routes/users.js";
 import { posts } from "./routes/posts.js";
 import cookieParser from "cookie-parser";
 
+import cors from 'cors';
+
 //Middleware 
 app.use(Express.json());
 app.use(cookieParser());
@@ -13,17 +15,19 @@ app.use(cookieParser());
 app.use(Express.static('public'));
 
 const corsOptions = {
-    origin : 'localhost', 
+    origin : 'http://localhost:3000', 
     credentials : true
 }
+
+app.use(cors(corsOptions));
 
 //Routes
 app.use('/api/auth', auth);
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 
-app.listen(4000, ()=>{
+app.listen(3000, ()=>{
 
-    console.log("Servidor en puerto 4000");
+    console.log("Servidor en puerto 3000");
 
 })
